640340b57c232ccb83086e2e760fc9b36d1d1ec9
Josh LaFayette <joshlafayette@gmail.com>
2018-08-05 06:38:40 -0700

    Add arrive behavior so vehicle does not overshoot

--- steering/vehicle.go
+++ steering/vehicle.go
@@ -7,24 +7,26 @@ import (
 
 // Vehicle calculates it's own desired force to get to a target.
 type Vehicle struct {
-	pos      pixel.Vec
-	acc      pixel.Vec
-	vel      pixel.Vec
-	tgt      *Target
-	maxSpeed float64
-	maxForce float64
+	pos          pixel.Vec
+	acc          pixel.Vec
+	vel          pixel.Vec
+	tgt          *Target
+	maxSpeed     float64
+	maxForce     float64
+	arriveRadius float64
 }
 
 // NewVehicle instantiates a new vehicle
 func NewVehicle(tgt *Target) Vehicle {
 
 	return Vehicle{
-		pos:      pixel.V(200, screenHeight/2),
-		acc:      pixel.V(0, 0),
-		vel:      pixel.V(0, 3),
-		tgt:      tgt,
-		maxSpeed: 3,
-		maxForce: .1,
+		pos:          pixel.V(200, screenHeight/2),
+		acc:          pixel.V(0, 0),
+		vel:          pixel.V(0, 3),
+		tgt:          tgt,
+		maxSpeed:     3,
+		maxForce:     .1,
+		arriveRadius: 50,
 	}
 }
 
@@ -36,7 +38,13 @@ func (v *Vehicle) update() {
 
 func (v *Vehicle) seek(tgt pixel.Vec) {
 	toTgt := v.pos.To(tgt)
-	desired := pixel.Unit(toTgt.Angle()).Scaled(v.maxSpeed)
+	var mag float64
+	if toTgt.Len() < v.arriveRadius { // arrive without overshooting
+		mag = mapRange(toTgt.Len(), 0, v.arriveRadius, 0, v.maxSpeed)
+	} else {
+		mag = v.maxSpeed
+	}
+	desired := pixel.Unit(toTgt.Angle()).Scaled(mag)
 	steering := desired.Sub(v.vel)
 	if steering.Len() > v.maxForce {
 		steering = pixel.Unit(steering.Angle()).Scaled(v.maxForce)
@@ -48,4 +56,11 @@ func (v *Vehicle) draw(imd *imdraw.IMDraw) {
 	// TODO: Use a sprite to draw this
 	imd.Push(v.pos)
 	imd.Circle(5, 0)
+}
+
+func mapRange(in, inMin, inMax, outMin, outMax float64) float64 {
+	// If in X falls between A and B, and you want output Y to
+	// fall between C and D, apply the following linear transform:
+	// Y = (X-A)/(B-A) * (D-C) + C
+	return (in-inMin)/(inMax-inMin)*(outMax-outMin) + outMin
 }
